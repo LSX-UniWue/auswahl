d54a2f299a2f44c5d8e7e9fa0067e4ea0f94e7e1
Stefan <stefan.heil@stud-mail.uni-wuerzburg.de>
2022-03-14 13:56:16 +0100

    Paralle operations for SPA

--- auswahl/tests/test_spa.py
+++ auswahl/tests/test_spa.py
@@ -7,21 +7,19 @@ from auswahl import SPA
 
 @pytest.fixture
 def data():
-    X = np.random.randn(100, 10)
-    y = 5 * X[:, 0] - 2 * X[:, 5]
+    X = np.array([[1, 0, 0, 3, 0],
+                  [0, 1, 0, 0, 2],
+                  [0, 0, 1, 0, 2]])
+    y = 5 * X[:, 3] - 2 * X[:, 2]
     return X, y
 
 
 def test_spa(data):
     X, y = data
 
-    vip = SPA(n_features_to_select=2)
+    spa = SPA(n_features_to_select=2, n_cv_folds=2)
 
-    vip.fit(X, y)
-    assert len(vip.support_) == X.shape[1]
-    assert sum(vip.support_) == 2
-    #assert_array_equal(vip.support_, [1, 0, 0, 0, 0, 1, 0, 0, 0, 0])
-
-    #X_t = vip.transform(X)
-    #assert X_t.shape[1] == 2
-    #assert_array_almost_equal(X[:, [0, 5]], X_t)
+    spa.fit(X, y)
+    assert len(spa.support_) == X.shape[1]
+    assert sum(spa.support_) == 2
+    #assert (spa.support_[3] == 1 and spa.support_[0] == 0)
