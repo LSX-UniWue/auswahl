1717a200e5618a5ab11a8d06de22d9e7f515b98f
Stefan <stefan.heil@stud-mail.uni-wuerzburg.de>
2022-03-22 17:35:30 +0100

    Updates syn-spectra generator

--- examples/utils/synthetic_data.py
+++ examples/utils/synthetic_data.py
@@ -18,10 +18,14 @@ class SyntheticGenerator:
         velo = np.diff(seed, 1)
         acc = np.diff(velo, 1)
 
-        n = 250 #  TODO: the generation process is probabilistic as of now, fix that with n_samples
+        n = 20 #  TODO: the generation process is probabilistic as of now, fix that with n_samples
 
-        # TODO: proof of concept
+        #proof of concept
         acc = acc * (0.2 * np.random.rand(n, acc.shape[0]) + 0.8)
+        acc_offset = (seed[-1] - seed[0]) - (velo[0] * (seed.shape[0] - 1))
+        acc_multiples = np.expand_dims(np.arange(seed.shape[0]-2, 0, -1), axis=0)
+        acc_norm = acc_offset / np.sum(acc*acc_multiples, axis=1)
+        acc = acc * np.expand_dims(acc_norm, axis=1)
 
         # TODO: switch to sampling with covariances to produce randomly selected correlation between wavelengths
         # cov = np.eye(seed.shape[0] - 2)
@@ -37,11 +41,12 @@ class SyntheticGenerator:
             velocity[:, i + 1] = velocity[:, i] + acc[:, i]
 
         trajectory = np.tile(seed, (n, 1))
+        trajectory[:, 0] = trajectory[:, 0] + 0.06 * np.random.rand(trajectory.shape[0])
         for i in range(velocity.shape[1]):
             trajectory[:, i + 1] = trajectory[:, i] + velocity[:, i]
 
-        diffs = np.abs(trajectory[:, -1] - seed[-1]) <= 0.05
-        trajectory = np.compress(diffs, trajectory, axis=0)
+        mask = np.all(trajectory >= 0, axis=1)
+        trajectory = np.compress(mask, trajectory, axis=0)
 
         return trajectory
 
@@ -61,8 +66,13 @@ def generate_2(seed):
     velo = np.diff(seed, 1)
     acc = np.diff(velo, 1)
 
-    n = 200
-    acc = acc * (0.2 * np.random.rand(n, acc.shape[0]) + 0.8)
+    n = 20
+    acc = acc * (0.1 * np.random.rand(n, acc.shape[0]) + 0.9)
+    acc_offset = (seed[-1] - seed[0]) - (velo[0] * (seed.shape[0] - 1))
+    acc_multiples = np.expand_dims(np.arange(seed.shape[0] - 2, 0, -1), axis=0)
+    acc_norm = acc_offset / np.sum(acc * acc_multiples, axis=1)
+    acc = acc * np.expand_dims(acc_norm, axis=1)
+
     #cov = np.eye(seed.shape[0] - 2)
     #sample = np.random.multivariate_normal(np.zeros((seed.shape[0]-2)), cov, size=n, check_valid='warn')
     #sample = np.array(sample).reshape((n, seed.shape[0]-2))
@@ -74,12 +84,13 @@ def generate_2(seed):
     for i in range(acc.shape[1]):
         velocity[:, i + 1] = velocity[:, i] + acc[:, i]
 
-    trajectory = np.tile(seed, (n,1))
+    trajectory = np.tile(seed, (n, 1))
+    trajectory[:, 0] = trajectory[:, 0] + 0.06 * np.random.rand(trajectory.shape[0])
     for i in range(velocity.shape[1]):
         trajectory[:, i + 1] = trajectory[:, i] + velocity[:, i]
 
-    diffs = np.abs(trajectory[:, -1] - seed[-1]) <= 0.05
-    trajectory = np.compress(diffs, trajectory, axis=0)
+    mask = np.all(trajectory >= 0, axis=1)
+    trajectory = np.compress(mask, trajectory, axis=0)
 
     print(f'Generated {trajectory.shape[0]} spectra')
     plot_spectra(np.arange(trajectory.shape[1]),
