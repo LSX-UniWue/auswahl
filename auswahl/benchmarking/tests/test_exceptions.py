61e47f9cd5be631e3f0a6cddb991af4243bc7d8f
Florian Buckermann <28018709+mcFloskel@users.noreply.github.com>
2022-09-02 12:28:49 +0200

    Updates exception tests for benchmarking

--- auswahl/benchmarking/tests/test_exceptions.py
+++ auswahl/benchmarking/tests/test_exceptions.py
@@ -1,104 +1,107 @@
 import numpy as np
 import pytest
-
 from sklearn.metrics import mean_squared_error
-from sklearn.cross_decomposition import PLSRegression
 
 from auswahl import VIP, IPLS
 from auswahl.benchmarking import deng_score, benchmark
 
 
-def _benchmark_interface_reduced_dof(data=[(np.zeros((50, 100)), np.zeros((50,)), 'test_dataset')],
+@pytest.fixture
+def data():
+    X = np.zeros((50, 10))
+    y = np.zeros((50,))
+    name = 'test_dataset'
+    test_size = 0.2
+    return X, y, name, test_size
+
+
+def _benchmark_interface_reduced_dof(data,
+                                     features=None,
                                      n_runs=1,
-                                     train_size=0.9,
-                                     reg_metrics=[mean_squared_error],
-                                     stab_metrics=[deng_score],
-                                     n_features=[1],
-                                     n_intervals=[1],
-                                     interval_widths=[1],
-                                     methods=[VIP(n_features_to_select=10),
-                                              IPLS(n_intervals_to_select=1, interval_width=10, n_jobs=1)]):
+                                     reg_metrics=None,
+                                     stab_metrics=None,
+                                     methods=None):
+    reg_metrics = [mean_squared_error] if reg_metrics is None else reg_metrics
+    stab_metrics = [deng_score] if stab_metrics is None else stab_metrics
+    features = [1] if features is None else features
+    methods = [VIP(n_features_to_select=10)] if methods is None else methods
 
     _ = benchmark(data=data,
+                  features=features,
                   n_runs=n_runs,
-                  train_size=train_size,
-                  test_model=PLSRegression(n_components=1),
                   reg_metrics=reg_metrics,
                   stab_metrics=stab_metrics,
                   methods=methods,
                   verbose=False,
-                  random_state=123456,
-                  n_features=n_features,
-                  n_intervals=n_intervals,
-                  interval_widths=interval_widths)
+                  random_state=123456)
 
 
-def test_n_features_exceptions():
-
+def test_n_features_exceptions(data):
     vip = VIP(n_features_to_select=10)
     ipls = IPLS(n_intervals_to_select=1, interval_width=10, n_jobs=1)
 
     params = [
-        # no interval configuration provided for IntervalSelector
-        {'n_features': [1], 'n_intervals': None, 'interval_widths': None, 'methods': [ipls]},
-        # incomplete specification of the total number of features
-        {'n_features': None, 'n_intervals': [1], 'interval_widths':None, 'methods': [ipls, vip]},
-        # incomplete specification of the total number of features
-        {'n_features': None, 'n_intervals': None, 'interval_widths': [1], 'methods': [vip]},
-        # inconsistent total number of features
-        {'n_features':[10, 12], 'n_intervals': [1, 1], 'interval_widths':[10, 13], 'methods': [vip, ipls]},
-        # inconsistent lengths
-        {'n_features': [10, 11, 12], 'n_intervals': [1, 1], 'interval_widths':[10, 11], 'methods': [vip, ipls]},
-        # inconsistent lengths
-        {'n_features': None, 'n_intervals':[1, 1], 'interval_widths':[10, 11, 12], 'methods': [vip]},
-        # n_intervals inconsistent with n_features (divisibility)
-        {'n_features': [10, 12], 'n_intervals': [1, 5], 'interval_widths':None, 'methods': [vip, ipls]}
+        {'features': [], 'methods': [vip]},  # nothing given for PointSelector
+        {'features': [], 'methods': [ipls]},  # nothing given for PointSelector
+
+        {'features': [(1, 1)], 'methods': [vip]},  # tuple given for PointSelector
+        {'features': [1], 'methods': [ipls]},  # Point given for IntervalSelector
+
+        {'features': [(1,)], 'methods': [vip]},  # wrong tuple given for PointSelector
+        {'features': [(1,)], 'methods': [ipls]},  # wrong tuple given for IntervalSelector
+        {'features': [(1, 2, 3)], 'methods': [ipls]},  # wrong tuple given for IntervalSelector
+
+        {'features': [10, (2, 5)], 'methods': [vip, ipls]}  # mixed PointSelector and IntervalSelector
     ]
 
     for i, param in enumerate(params):
         print(f'{i}')
         with pytest.raises(ValueError):
-            _benchmark_interface_reduced_dof(**param)
+            _benchmark_interface_reduced_dof(data=data, **param)
 
 
-def test_dataset_exceptions():
+def test_dataset_exceptions(data):
+    X, y, name, test_size = data
 
-    x = np.zeros((50, 100))
-    y = np.zeros((50,))
+    erroneous_data = [
+        [data, data],  # non-unique names
+        [(X, y, name)],  # not all fields specified
+        [(10, 11, name, 0.2)],  # wrong data type
+        [(X, y, 10, 0.2)],  # wrong data type
+        [(X, y, name, '0.2')]  # wrong data type
+    ]
 
-    params = [{'data': [(x, y, 'test'), (x, y, 'test')]},  # non-unique names
-              {'data': [(x, y)]},  # not all fields specified
-              {'data': [(10, 11, 'test')]},  # wrong data type
-              {'data': [(x, y, 10)]}]  # wrong data type
+    for i, d in enumerate(erroneous_data):
+        print(f'{i}')
+        with pytest.raises(ValueError):
+            _benchmark_interface_reduced_dof(data=d)
+
+
+def test_metrics(data):
+    params = [
+        {'reg_metrics': [mean_squared_error, mean_squared_error]},  # names not unique
+        {'stab_metrics': [deng_score, deng_score]},  # names not unique
+        {'reg_metrics': []},  # no regression metric specified
+    ]
 
     for i, param in enumerate(params):
         print(f'{i}')
         with pytest.raises(ValueError):
-            _benchmark_interface_reduced_dof(**param)
+            _benchmark_interface_reduced_dof(data=data, **param)
 
 
-def test_metrics():
+def test_train_size(data):
+    X, y, name, test_size = data
 
-    params = [{'reg_metrics': [mean_squared_error, mean_squared_error]},  # names not unique
-              {'stab_metrics': [deng_score, deng_score]},  # names not unique
-              {'reg_metrics': []}]  # no regression metric specified
+    erroneous_data = [
+        [(X, y, name, 0.0)],  # test_size equals zero
+        [(X, y, name, 1.0)],  # test_size equals n_samples
+        [(X, y, name, 0.01)],  # test_size higher than 0.0 but to small
+        [(X, y, name, 0.99)],  # test_size lower than 1.0 but to large
+        [(X, y, name, 1.1)]  # test_size to large
+    ]
 
-    for i, param in enumerate(params):
+    for i, d in enumerate(erroneous_data):
         print(f'{i}')
         with pytest.raises(ValueError):
-            _benchmark_interface_reduced_dof(**param)
-
-
-def test_train_size():
-
-    data = (np.zeros((10, 100)), np.zeros((10,)), 'test_dataset')
-
-    params = [{'train_size': [0.5, 0.5]},  # length not matching #datasets
-              {'train_size': 0},  # out of valid range
-              {'train_size': 1},   # out of valid range
-              {'data': data, 'train_size': 0.95}]  # invalid train_size w.r.t. dataset size
-
-    for i, param in enumerate(params):
-        print(f'{i}')
-        with pytest.raises(ValueError):
-            _benchmark_interface_reduced_dof(**param)
+            _benchmark_interface_reduced_dof(data=d)
