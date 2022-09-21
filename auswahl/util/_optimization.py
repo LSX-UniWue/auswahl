1c000bcf13fa56196e5ff579a64bb40d554007e7
Stefan <stefan.heil@stud-mail.uni-wuerzburg.de>
2022-09-21 18:05:19 +0200

    Improves interval optimization

--- auswahl/util/_optimization.py
+++ auswahl/util/_optimization.py
@@ -6,7 +6,8 @@ def optimize_intervals(n_intervals: int, interval_width: int, feature_scores: np
     """The algorithm calculates the optimal non-overlapping placement of n_intervals of width interval_width into the
     range of features. The feature scores are specified in feature_scores (greater better). The algorithm can be used
     for instance to turn every point selection algorithm yielding a score for each feature
-    (such as :class:`~auswahl.VIP`) into an interval selection algorithm.
+    (such as :class:`~auswahl.VIP`) into an interval selection algorithm. The runtime of the algorithm is :math:`O(kn)`, with
+    k being n_intervals and n feature_scores.size
 
     Parameters
     ----------
@@ -42,23 +43,24 @@ def optimize_intervals(n_intervals: int, interval_width: int, feature_scores: np
                          f'interval_width. Required at least {n_intervals * interval_width}, got {feature_scores.size}')
 
     # costs[i]: total score of the interval starting at i
-    cumsum = np.concatenate([np.array([0]), np.cumsum(feature_scores)])
-    intervals = sliding_window_view(cumsum, interval_width + 1)
-    interval_scores = intervals[:, -1] - intervals[:, 0]
+    intervals = sliding_window_view(feature_scores, interval_width)
+    interval_scores = np.sum(intervals, axis=1)
 
-    # table[i, k] specifies the score of allocating in the range {0, ..., i} k intervals
-    # initalize with sentinels
-    table = -1000000 * np.ones((feature_scores.size, n_intervals + 1))
+    # table[i, k] specifies the score of allocating in the range of features {0, ..., i} k intervals
+    # initialize with sentinels
+    # introduce index -1 sentinel into the first axis of the table
+    table = -1000000 * np.ones((feature_scores.size + 1, n_intervals + 1))
     table[:, 0] = 0  # zero costs for no intervals
     interval_starts = [[[] for _ in range(n_intervals + 1)] for _ in range(feature_scores.size)]
 
     for i in range(interval_width - 1, feature_scores.size):
         for k in range(1, min((i + 1) // interval_width, n_intervals) + 1):
-            # iterate through placements of the last interval in the considered interval
-            scores = [(p, interval_scores[p] + table[p - 1, k - 1]) for p in range(i - interval_width + 1,
-                                                                                   (k - 1) * interval_width - 1, -1)]
-            pos, score = max(scores, key=lambda x: x[1])
-            table[i, k] = score
-            interval_starts[i][k] = interval_starts[pos - 1][k - 1] + [pos]
+            score_incl_i = interval_scores[i - interval_width + 1] + table[i - interval_width, k - 1]
+            if score_incl_i > table[i-1, k]:
+                table[i, k] = score_incl_i
+                interval_starts[i][k] = interval_starts[i - interval_width][k - 1] + [i - interval_width + 1]
+            else:
+                table[i, k] = table[i - 1, k]
+                interval_starts[i][k] = interval_starts[i - 1][k].copy()
 
     return table[feature_scores.size - 1, n_intervals], interval_starts[feature_scores.size - 1][n_intervals]
